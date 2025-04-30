7c51a25cad9b99b0bf1dcaa0814c9f8bf45fea67
trr0jan <maksym.nesterchuk@lyceum3.mk.ua>
2025-04-30 14:35:27 +0300

    test-alpha1

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("menuButton");
-
-    menuButton.addEventListener("click", function (event) {
-        if (event.detail === 1) {
-            window.location.href = "index.html";
-        }
-    });
-});
